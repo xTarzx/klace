794b5bbfddd8c6ba06e1d6831eb680705bbde943
xTarzx <telmomartins2000@gmail.com>
2025-02-17 18:31:48 +0000

    wow

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,41 @@
 const std = @import("std");
 
-pub fn main() !void {}
+const c = @cImport({
+    @cInclude("SDL3/SDL.h");
+});
+
+pub fn main() !void {
+    std.debug.print("SDL: {d}.{d}.{d}\n", .{
+        c.SDL_MAJOR_VERSION,
+        c.SDL_MINOR_VERSION,
+        c.SDL_MICRO_VERSION,
+    });
+
+    if (!c.SDL_Init(c.SDL_INIT_VIDEO)) {
+        std.debug.print("SDL_Init failed: {s}\n", .{c.SDL_GetError()});
+        return;
+    }
+    defer c.SDL_Quit();
+
+
+
+    const window_width = 800;
+    const window_height = 600;
+    const window_flags = 0;
+    const window = c.SDL_CreateWindow("klace", window_width, window_height, window_flags);
+    const renderer = c.SDL_CreateRenderer(window, "klace");
+
+    var running = true;
+    while (running) {
+        var event: c.SDL_Event;
+        while (c.SDL_PollEvent(&event)) {
+            switch (event.type) {
+                c.SDL_QUIT => running = false,
+            }
+        }
+
+        c.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        c.SDL_RenderClear(renderer);
+    }
+
+}
